b1204d89369066f45ed23258dea4bf0df79abdee
TestUser46543 <89195449+TestUser46543@users.noreply.github.com>
2021-09-13 00:38:10 +0530

    Update buildImages.sh

--- buildImages.sh
+++ buildImages.sh
@@ -1,12 +1,12 @@
 echo "Building all Dockerfiles"
 cd vote
-docker build -t akshat1902/vote-app .
-docker login -u "akshat1902" -p "Akshat@2417" docker.io
-docker push akshat1902/vote-app
+docker build -t docker1234432156/vote-app .
+docker login -u "docker1234432156" -p "password123" docker.io
+docker push docker1234432156/vote-app
 cd ..
 cd result
-docker build -t akshat1902/result-app .
-docker push akshat1902/result-app
+docker build -t docker1234432156/result-app .
+docker push docker1234432156/result-app
 docker rm -f redis db vote-app result-app worker-app
 cd ..
 docker logout
